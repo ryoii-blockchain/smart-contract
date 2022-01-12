c0c6bcd63ae9d7c76a7589aa91a1df38a1db7f06
Bank Pitipong <87498474+ChipJaa@users.noreply.github.com>
2022-01-12 15:23:43 +0700

    Current Version

--- RyoiiNFT-ERC721.sol
+++ RyoiiNFT-ERC721.sol
@@ -2,23 +2,46 @@
 pragma solidity ^0.8.2;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Burnable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 
-contract RyoiiNFT is ERC721, ERC721Burnable, Ownable {
+contract RyoiiNFT is ERC721, ERC721URIStorage, ERC721Burnable, Ownable {
     using Counters for Counters.Counter;
 
     Counters.Counter private _tokenIdCounter;
+    mapping(uint256 => uint) couponUsed;
 
-    constructor() ERC721("Ryoii NFT", "RNFT") {}
+    constructor() ERC721("Ryoii NFT Coupon", "RNFT") {}
 
-    function safeMint(address to) public onlyOwner {
-        _safeMint(to, _tokenIdCounter.current());
+    function safeMint(address to, string memory uri) public onlyOwner {
+        uint256 tokenId = _tokenIdCounter.current();
+        couponUsed[tokenId] = 0;
         _tokenIdCounter.increment();
+        _safeMint(to, tokenId);
+        _setTokenURI(tokenId, uri);
     }
 
-    function _baseURI() internal pure override returns (string memory) {
-        return "http://159.65.137.25:4001/nfts/";
+    function _burn(uint256 tokenId) internal override(ERC721, ERC721URIStorage) {
+        super._burn(tokenId);
     }
+
+    function tokenURI(uint256 tokenId)
+        public
+        view
+        override(ERC721, ERC721URIStorage)
+        returns (string memory)
+    {
+        return super.tokenURI(tokenId);
+    }
+
+    function setCouponUsageStatus(uint256 index, uint value) public onlyOwner {
+        couponUsed[index] = value;
+    }
+
+    function getCouponUsageStatus(uint256 index)  public view returns (uint) {
+        return couponUsed[index];
+    }
+    
 }
