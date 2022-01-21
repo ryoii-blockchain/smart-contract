d062dad88d0312b63a839811073cf507d6d3cb61
Chonnarong Hanyawongse <thenongt@gmail.com>
2022-01-21 11:02:40 +0700

    write contracts artifact to file after deployed

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,3 +1,5 @@
+import fs from 'fs';
+
 const RyoiiCoin = artifacts.require("RyoiiCoin");
 const RyoiiNFT = artifacts.require("RyoiiNFT");
 const ERC721 = artifacts.require("ERC721");
@@ -5,8 +7,14 @@ const ERC721URIStorage = artifacts.require("ERC721URIStorage");
 const ERC721Burnable = artifacts.require("ERC721Burnable");
 const Ownable = artifacts.require("Ownable");
 
-module.exports = function(deployer) {
-  deployer.deploy(RyoiiCoin);
-  deployer.deploy(RyoiiNFT);
+module.exports = async function(deployer) {
+  await deployer.deploy(RyoiiCoin);
+  await deployer.deploy(RyoiiNFT);
+
+  fs.writeFile('../truffle_data/contracts.json', JSON.stringify({
+    ryc: RyoiiCoin.address,
+    nft: RyoiiNFT.address,
+  }));
+
   deployer.link(RyoiiNFT, [ERC721, ERC721URIStorage, ERC721Burnable, Ownable]);
 };
